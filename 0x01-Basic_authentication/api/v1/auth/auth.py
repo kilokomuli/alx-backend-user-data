475196deac0ce517ce25ee4121315928a5390840
kilokomuli <kilokoemma175@gmail.com>
2024-08-06 16:14:00 +0000

    task4

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -7,8 +7,17 @@ from typing import List, TypeVar
 class Auth:
     """Auth class"""
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """Public method"""
-        return False
+        """Defines routes that dont need authentication"""
+        if path is None:
+            return True
+        if not excluded_paths:
+            return True
+        if not path.endswith('/'):
+            path += '/'
+        for excluded_path in excluded_paths:
+            if excluded_path.endswith('/') and path == excluded_path:
+                return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """Flask request object"""
