21244d1ae749d955a2ae001aa18e838cef322876
kilokomuli <kilokoemma175@gmail.com>
2024-08-08 10:49:33 +0000

    update sessionauth

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -2,8 +2,24 @@
 """SessionAuth"""
 from api.v1.auth.auth import Auth
 from flask import Flask
+import uuid
 
 
 class SessionAuth(Auth):
     """Inherits from Auth"""
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """Instance method creates a Session ID for a user_id"""
+        if user_id is None or not isinstance(user_id, str):
+            return None
+        session_id = str(uuid.uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """Retrieves a link between a User ID and a session ID"""
+        if session_id is None or not isinstance(session_id, str):
+            return None
+
+        return self.user_id_by_session_id.get(session_id)
