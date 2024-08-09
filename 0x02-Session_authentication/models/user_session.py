6be09804194676a94a215d7129ca847d6e28fbf4
kilokomuli <kilokoemma175@gmail.com>
2024-08-09 21:53:47 +0800

    implented usersession

--- 0x02-Session_authentication/models/user_session.py
+++ 0x02-Session_authentication/models/user_session.py
@@ -1,1 +1,12 @@
 #!/usr/bin/env python3
+"""UserSession module"""
+from models.base import Base
+
+
+class UserSession(Base):
+    """Class UserSession that inherits from Base"""
+    def __init__(self, *args: list, **kwargs: dict):
+        """class method"""
+        super().__init__(*args, **kwargs)
+        self.user_id = kwargs.get('user_id')
+        self.session_id = kwargs.get('session_id')
