dd78ef23c8c90dd20116ed8c2ed451aa452f4b6a
kilokomuli <kilokoemma175@gmail.com>
2024-08-13 20:11:35 +0000

    fix linter

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -30,7 +30,7 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
-    def add_user(self, email: str, hashed_password:str) -> User:
+    def add_user(self, email: str, hashed_password: str) -> User:
         """returnns a user object"""
         new_user = User(email=email, hashed_password=hashed_password)
         try:
