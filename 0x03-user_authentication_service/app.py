9a2985dc71028332244d9e0248f208aa9ffe8edf
kilokomuli <kilokoemma175@gmail.com>
2024-08-14 20:08:47 +0000

    task6

---
+++ 0x03-user_authentication_service/app.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""Routing module"""
+from flask import Flask, jsonify
+
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+app.config["JSONIFY_PRETTYPRINT_REGULAR"] = True
+
+
+@app.route("/")
+def home() -> str:
+    """Basic route"""
+    return jsonify({"message": "Bienvenue"})
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
