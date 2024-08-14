229e616bafb4a836e364c2c8bb603f104f15679d
kilokomuli <kilokoemma175@gmail.com>
2024-08-14 19:51:41 +0000

    task4

---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""hash password"""
+import bcrypt
+from user import User
+from db import DB
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+def _hash_password(password: str) -> bytes:
+    """Returns bytes as a salted hash of the input
+    password"""
+    e_pwd = password.encode()
+    return bcrypt.hashpw(e_pwd, bcrypt.gensalt())
