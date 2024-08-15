531b53c29a58984fbdf6fd4c4daf891c6bc43c6f
kilokomuli <kilokoemma175@gmail.com>
2024-08-15 02:27:10 +0000

    register user

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,17 +1,30 @@
 #!/usr/bin/env python3
 """Routing module"""
-from flask import Flask, jsonify
+from flask import Flask, jsonify, abort, request
+from auth import Auth
 
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 app.config["JSONIFY_PRETTYPRINT_REGULAR"] = True
+AUTH = Auth()
 
 
 @app.route("/")
 def home() -> str:
     """Basic route"""
     return jsonify({"message": "Bienvenue"})
+
+@app.route("/users", methods=["POST"])
+def users():
+    """Registers user"""
+    email = request.form.get("email")
+    password = request.form.get("password")
+    try:
+        AUTH.register_user(email, password)
+        return jsonify({"email": email, "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
