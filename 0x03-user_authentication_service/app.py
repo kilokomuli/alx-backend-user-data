89029154a90b387aced613532405c2e7de639e32
kilokomuli <kilokoemma175@gmail.com>
2024-08-16 01:54:13 +0000

    user

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -53,5 +53,15 @@ def logout():
     return redirect(url_for("home"))
 
 
+@app.route("/profille", methods=["GET"])
+def profile():
+    """user profile"""
+    session_id = request.cookies.get("session_id")
+    user = AUTH.get_user_from_session_id(session_id)
+    if not user:
+        abort(403)
+    return jsonify({"email": user.email})
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
